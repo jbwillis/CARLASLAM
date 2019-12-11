e8ec9dcd66cbbc8d861ffe53aa9d937d45f513f1
Jacob Willis <jbwillis272@gmail.com>
2019-12-11 16:48:33 -0700

    started gMapping base algorithm

--- gMapping.py
+++ gMapping.py
@@ -1,4 +1,29 @@
 # top level algorithm for gMapping SLAM
+from MotionModel import modelStep
+from map import *
+
+def runStep(particle_set_tm1, scan_t, odom_tm1):
+    
+    particle_set_t  = []
+
+    # convert scan into occupancy grid map
+
+    gridMapFromScan(scan, radius):
+
+    for particle in particle_set_tm1:
+        
+        # particle has:
+        # particle.pose
+        # particle.weight
+        # particle.map
+
+        #################################
+        # scan matching
+        x_t_pr  = modelStep(particle.pose, odom_tm1) # propagate the particle's state forward
+
+        x_t_hat = scanmatch(subliklihoodfield, scan_t, x_t_pr)
+
+
 
 
 
